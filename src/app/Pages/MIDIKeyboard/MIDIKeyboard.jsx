51a7c10da4ae14595cbc322ec205ef7e622c1791
Ryan Yang <ryanqyang@gmail.com>
2020-10-26 02:30:50 -0700

    Finished functionality with the midi keyboard. Now looking to customize each button

--- src/app/Pages/MIDIKeyboard/MIDIKeyboard.jsx
+++ src/app/Pages/MIDIKeyboard/MIDIKeyboard.jsx
@@ -1,13 +1,28 @@
 import {useContext} from 'react';
 import UserSoundMappingContext from '../../controller/contexts/UserSoundMappingContext';
+import './MIDIKeyboard.scss';
 
+/**
+ * Using index as key here is ok since array should be constant. May look to change this feature if we 
+ * consider expanding functionality
+ */
 const MIDIKeyboard = () => {
     const [userSoundMapping] = useContext(UserSoundMappingContext);
-    console.log(userSoundMapping);
-    return (
-        <div className="MIDIKeyboard--wrapper">
-            <div className="grid">
+    
+    console.log("Loading user sound mapping context: ", userSoundMapping);
 
+    const unsetBinding = () => {
+        console.log("Binding has not been set!");
+    }
+
+    return (
+        <div className="midi-keyboard--wrapper">
+            <div className="grid">
+                {userSoundMapping.map((e, i) => (
+                    // <div className="">
+                        <button className="key" key={i} onTouchStart={e ?? unsetBinding}></button>
+                    // </div>
+                ))}
             </div>
         </div>
     )
--- src/app/controller/contexts/UserSoundMappingContext.js
+++ src/app/controller/contexts/UserSoundMappingContext.js
@@ -1,9 +1,30 @@
 import {createContext} from 'react';
 
+import * as Tone from 'tone';
+
+const synth = new Tone.PolySynth(Tone.Synth).toDestination();
 const UserSoundMappingContext = createContext(); 
 
 const TOTAL_KEYS = 16; // ideally a perfect square
-const defaultSoundMapping = Array.from(new Array(TOTAL_KEYS), (e, i) => i);
+// const defaultSoundMapping = new Array(TOTAL_KEYS).fill(null);
+const defaultSoundMapping = [
+    () => { synth.triggerAttackRelease("C3", "8n") },
+    () => { synth.triggerAttackRelease("D3", "8n") },
+    () => { synth.triggerAttackRelease("E3", "8n") },
+    () => { synth.triggerAttackRelease("F3", "8n") },
+    () => { synth.triggerAttackRelease("G3", "8n") },
+    () => { synth.triggerAttackRelease("A3", "8n") },
+    () => { synth.triggerAttackRelease("B3", "8n") },
+    () => { synth.triggerAttackRelease("C4", "8n") },
+    () => { synth.triggerAttackRelease("D4", "8n") },
+    () => { synth.triggerAttackRelease("E4", "8n") },
+    () => { synth.triggerAttackRelease("F4", "8n") },
+    () => { synth.triggerAttackRelease("G4", "8n") },
+    () => { synth.triggerAttackRelease("A4", "8n") },
+    () => { synth.triggerAttackRelease("B4", "8n") },
+    () => { synth.triggerAttackRelease("C5", "8n") },
+    () => { synth.triggerAttackRelease("D5", "8n") },
+];
 
 export default UserSoundMappingContext;
 export {defaultSoundMapping}
