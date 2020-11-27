48964f3d7b0270fee8bbe139359484f42c83d556
Ryan Yang <ryanqyang@gmail.com>
2020-11-27 00:03:48 -0800

    adding song list display screen and linking it to the track editor

--- src/app/Pages/SongListDisplay/SongListDisplay.jsx
+++ src/app/Pages/SongListDisplay/SongListDisplay.jsx
@@ -1,5 +1,7 @@
 import {useContext} from 'react';
+import {Link} from 'react-router-dom';
 import { Card } from '../../components/basic';
+import GeoPattern from 'geopattern';
 import UserSongListContext from '../../controller/contexts/UserSongsListContext';
 import './SongListDisplay.scss';
 
@@ -7,25 +9,40 @@ const SongListDisplay = () => {
     const songlist = useContext(UserSongListContext);
     return (
         <div className="song-list--wrapper">
-            {songlist.map(song => (
-                <Card key={song.uid}>
-                    <div className="song-list--item">
-                        {song.name}
-                        {song.credit}
-                        <div className="date-information">
-                            <span>Last Edited:</span>
-                            <span>Created:</span>
-                            <p>{new Date(song.dateEdited).toDateString()}</p>
-                            <p>{new Date(song.dateCreated).toDateString()}</p>
-                        </div>
+            <div className="list--wrapper">
+                <h3>Songs <span>({songlist.length})</span></h3>
+                <div className="list">
+                    <div className="list--item">
+                        <Card className="icon create-new" key="@ryqndev/new"></Card>
+                        <h4>Create New</h4>
                     </div>
-                </Card>
-            ))}
+                    {songlist.map(song => (
+                        <Link to={`/song/${song.uid}`}>
+                            <div className="list--item">
+                                <Card className="icon" key={song.uid} style={{backgroundImage: GeoPattern.generate(song.name).toDataUrl()}}></Card>
+                                <h4>{song.name}</h4>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
 
 export default SongListDisplay;
+
+// {/* <div className="song-list--item">
+// {song.name}
+// {song.credit}
+// <div className="date-information">
+//     <span>Last Edited:</span>
+//     <span>Created:</span>
+//     <p>{new Date(song.dateEdited).toDateString()}</p>
+//     <p>{new Date(song.dateCreated).toDateString()}</p>
+// </div>
+// </div> */}
+
 
 // name: 'End of it (Cover)',
 //     creator: 'Ryan Yang',
---
+++ src/app/controller/userDataStore.js
@@ -0,0 +1,13 @@
+import SAMPLE_SONG from '../assets/end-of-it-friday-pilots-club.json';
+
+const data = {
+    songs: [
+        SAMPLE_SONG
+    ]
+};
+
+
+
+export {
+    data
+}
